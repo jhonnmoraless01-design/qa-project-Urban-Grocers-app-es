747dd2a345835214af159edb85cf74f180c317a9
jhonnmoraless01-design <jhonn.moraless01@gmail.com>
2026-07-22 20:41:50 -0500

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-716664c2-5fd9-4ebe-a624-bd23c73134c3.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-63851d74-d559-46a4-87f6-314f5a44859c.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
