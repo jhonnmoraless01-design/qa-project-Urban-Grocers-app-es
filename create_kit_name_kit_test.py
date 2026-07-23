5919304143492729d162cbd1747133af5587a212
jhonnmoraless01-design <jhonn.moraless01@gmail.com>
2026-07-22 20:40:32 -0500

    Update create_kit_name_kit_test.py

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -18,38 +18,38 @@ def negative_assert_code_400(kit_body):
     response = sender_stand_request.post_new_client(kit_body, get_new_user_token())
     assert  response.status_code == 400
 
-def test_1_kit_name_with_1_letter(): #Expected: 201, Actual:400
-    new_kit_body = get_kit_body("a")
+def test_1_kit_name_with_1_letter():
+    new_kit_body = get_kit_body(data.with_one_letter)
     positive_assert(new_kit_body)
 
-def test_2_kit_name_with_511_letters(): #Expected: 201, Actual:400
-    new_kit_body = get_kit_body("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC")
+def test_2_kit_name_with_511_letters():
+    new_kit_body = get_kit_body(data.with_511_letters)
     positive_assert(new_kit_body)
 
-def test_3_kit_name_with_0_letter(): #PASSED
-    new_kit_body = get_kit_body("")
+def test_3_kit_name_with_0_letter():
+    new_kit_body = get_kit_body(data.with_0_letters)
     negative_assert_code_400(new_kit_body)
 
-def test_4_kit_name_with_512_letters(): #PASSED
-    new_kit_body = get_kit_body("AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD")
+def test_4_kit_name_with_512_letters():
+    new_kit_body = get_kit_body(data.with_512_letters)
     negative_assert_code_400(new_kit_body)
 
-def test_5_kit_name_with_special_characters(): #Expected: 201, Actual:400
-    new_kit_body = get_kit_body("*%@+,")
+def test_5_kit_name_with_special_characters():
+    new_kit_body = get_kit_body(data.with_special_characters)
     positive_assert(new_kit_body)
 
-def test_6_kit_name_allowed_with_a_space(): #Expected: 201, Actual:400
-    new_kit_body = get_kit_body(" A aa ")
+def test_6_kit_name_allowed_with_a_space():
+    new_kit_body = get_kit_body(data.with_a_space)
     positive_assert(new_kit_body)
 
-def test_7_kit_name_with_numbers(): #Expected: 201, Actual:400
-    new_kit_body = get_kit_body("123")
+def test_7_kit_name_with_numbers():
+    new_kit_body = get_kit_body(data.with_numbers_str)
     positive_assert(new_kit_body)
 
-def test_8_without_the_kit_name_parameter(): #PASSED
-    new_kit_body = get_kit_body({})
+def test_8_without_the_kit_name_parameter():
+    new_kit_body = get_kit_body(data.without_any_parameters)
     negative_assert_code_400(new_kit_body)
 
-def test_9__kit_name_with_numbers(): #PASSED
-    new_kit_body = get_kit_body(123)
+def test_9__kit_name_with_numbers():
+    new_kit_body = get_kit_body(data.with_numbers)
     negative_assert_code_400(new_kit_body)
